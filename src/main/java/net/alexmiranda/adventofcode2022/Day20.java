9c9cb1ae8b2286bf66496912040fa7301a101c6b
Alex Miranda <alexmirandamoraes@gmail.com>
2022-12-29 02:53:24 +0100

    Day 20 - part 2

--- src/main/java/net/alexmiranda/adventofcode2022/Day20.java
+++ src/main/java/net/alexmiranda/adventofcode2022/Day20.java
@@ -9,20 +9,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class Day20 {
+    static final long DECRYPTION_KEY = 811589153L;
+
     private static final String INPUT = "/2022/day/20/input";
     private static final int[] GROVE_COORDS = new int[] { 1000, 2000, 3000 };
 
-    record Number(int intValue, int originalPosition) {
+    record Number(long longValue, int originalPosition) {
         @Override
         public String toString() {
-            return Integer.toString(intValue);
+            return Long.toString(longValue);
         }
     }
 
-    static int answerPart1(List<Integer> list) {
+    static long sumOfGroveCoordinates(List<Long> list) {
         int zeroPos = -1;
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).intValue() == 0) {
+            if (list.get(i).equals(0L)) {
                 zeroPos = i;
                 break;
             }
@@ -30,7 +32,7 @@ public class Day20 {
 
         assert zeroPos >= 0;
 
-        var sum = 0;
+        var sum = 0L;
         for (var pos : GROVE_COORDS) {
             int actualPos = (zeroPos + pos) % list.size();
             sum += list.get(actualPos);
@@ -38,50 +40,62 @@ public class Day20 {
         return sum;
     }
 
-    static List<Integer> mix(List<Integer> file) {
+    static List<Long> mix(List<Long> file, int rounds) {
         int n = file.size();
         var input = new ArrayList<Number>(n);
         for (int i = 0; i < n; i++) {
             input.add(new Number(file.get(i), i));
         }
 
-        for (int i = 0; i < n; i++) {
-            var from = input.indexOf(new Number(file.get(i), i));
-            var num = input.get(from);
-            if (num.intValue == 0) {
-                continue;
-            }
+        while (rounds > 0) {
+            rounds--;
 
-            int to = targetPosition(from, num.intValue, n);
-            if (from == to) {
-                continue;
-            }
+            for (int i = 0; i < n; i++) {
+                var from = input.indexOf(new Number(file.get(i), i));
+                var num = input.get(from);
+                if (num.longValue == 0) {
+                    continue;
+                }
 
-            var small = Math.min(from, to);
-            var big = Math.max(from, to);
-            Collections.rotate(input.subList(small, big + 1), from < to ? -1 : 1);
+                int to = targetPosition(from, num.longValue, n);
+                if (from == to) {
+                    continue;
+                }
+
+                var small = Math.min(from, to);
+                var big = Math.max(from, to);
+                Collections.rotate(input.subList(small, big + 1), from < to ? -1 : 1);
+            }
         }
 
-        return input.stream().mapToInt(v -> v.intValue).boxed().toList();
+        return input.stream().mapToLong(v -> v.longValue).boxed().toList();
     }
 
-    static int targetPosition(int pos, int value, int n) {
-        int dest = pos + value;
+    static int targetPosition(int pos, long value, int n) {
+        long dest = pos + value;
         if (dest >= n) {
             dest %= n - 1;
         } else if (dest < 0) {
             dest %= n - 1;
             dest += n - 1;
         }
-        return dest;
+        return (int) dest;
     }
 
-    static List<Integer> mix(Reader reader, int n) throws IOException {
+    static List<Long> mix(Reader reader, int n, int rounds, long decryptionKey) throws IOException {
         try (var br = new BufferedReader(reader)) {
-            var file = new ArrayList<Integer>(n);
-            br.lines().limit(n).mapToInt(Integer::parseInt).forEach(file::add);
-            return mix(file);
+            var file = new ArrayList<Long>(n);
+            br.lines().limit(n).mapToLong(Long::parseLong)
+                    .map(longValue -> Math.multiplyExact(longValue, decryptionKey)).forEach(file::add);
+            return mix(Collections.unmodifiableList(file), rounds);
         }
+    }
+
+    static List<Long> applyDecryptionKey(List<Long> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(i) * DECRYPTION_KEY);
+        }
+        return list;
     }
 
     static InputStreamReader puzzleInput() {
--- src/test/java/net/alexmiranda/adventofcode2022/Day20Test.java
+++ src/test/java/net/alexmiranda/adventofcode2022/Day20Test.java
@@ -6,19 +6,35 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class Day20Test {
+    private static final int NO_DECRYPTION_KEY = 1;
+
     @Test
     public void testExamplePart1() {
-        var file = Arrays.asList(1, 2, -3, 3, -2, 0, 4);
+        var file = Arrays.asList(1L, 2L, -3L, 3L, -2L, 0L, 4L);
         // var expected = Arrays.asList(1, 2, -3, 4, 0, 3, -2);
-        var mixed = Day20.mix(file);
+        var mixed = Day20.mix(file, 1);
         // assertIterableEquals(expected, mixed);
-        assertEquals(3, Day20.answerPart1(mixed));
+        assertEquals(3, Day20.sumOfGroveCoordinates(mixed));
     }
 
     @Test
     public void testPuzzleInputPart1() throws IOException {
         var file = Day20.puzzleInput();
-        var mixed = Day20.mix(file, 5000);
-        assertEquals(988, Day20.answerPart1(mixed));
+        var mixed = Day20.mix(file, 5000, 1, NO_DECRYPTION_KEY);
+        assertEquals(988, Day20.sumOfGroveCoordinates(mixed));
+    }
+
+    @Test
+    public void testExamplePart2() {
+        var file = Day20.applyDecryptionKey(Arrays.asList(1L, 2L, -3L, 3L, -2L, 0L, 4L));
+        var mixed = Day20.mix(file, 10);
+        assertEquals(1623178306L, Day20.sumOfGroveCoordinates(mixed));
+    }
+
+    @Test
+    public void testPuzzleInputPart2() throws IOException {
+        var file = Day20.puzzleInput();
+        var mixed = Day20.mix(file, 5000, 10, Day20.DECRYPTION_KEY);
+        assertEquals(7768531372516L, Day20.sumOfGroveCoordinates(mixed));
     }
 }
